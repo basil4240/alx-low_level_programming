861ad0ac38268cc4372360dc1120dfca137fadc9
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-12 06:22:44 -0700

    made corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,15 +7,13 @@
  *
  * Return: Always (Success)
  */
-
 int main(void)
 {
 	int n;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -31,3 +29,4 @@ int main(void)
 
 	return (0);
 }
+
