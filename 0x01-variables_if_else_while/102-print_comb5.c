e50ce52a1b3b14e5cf9ff529f41521148f719aa3
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-12 07:21:29 -0700

    new

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,28 +9,30 @@
 
 int main(void)
 {
-	int i, p;
-	for (i = 0; i < 100; i++)
-	{
-		for (p = 0; p < 100; j++)
-		{
-			if (p > i)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+    int i, p;
 
-	putchar('\n');
-	return (0);
+    for (i = 0; i < 100; i++)
+    {
+        for (p = 0; p < 100; p++)
+        {
+	    if (p > i)
+	    {
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+	        putchar(' ');
+		putchar((p / 10) + '0');
+		putchar((p % 10) + '0');
+		if (i != 98)
+		{
+		    putchar(',');
+		    putchar(' ');
+		}
+	    }
+        }
+    }
+
+    putchar('\n');
+    return (0);
 
 }
+
