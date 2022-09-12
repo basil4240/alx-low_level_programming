2b35592bd89691aa75be5fe5ccbe69d25ca9f083
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-12 07:09:21 -0700

    made corrections

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - print possible combo of 3
  *
@@ -15,17 +16,17 @@ int main(void)
 		{
 			for (m = 2; m <= 9; m++)
 			{
-                if (m > p && p > i)
-                {
-                    putchar(i + '0');
-                    putchar(p + '0');
-                    putchar(m + '0');
-                        if (i != 7)
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                }
+                		if (m > p && p > i)
+                		{
+                    			putchar(i + '0');
+                    			putchar(p + '0');
+                    			putchar(m + '0');
+                        		if (i != 7)
+                        		{
+		                            putchar(',');
+                		            putchar(' ');
+                        		}
+                		}
 			}
 		}
 	}
