54ec214c082cb05ac34389341937ef6f52eb2c63
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-13 19:44:53 -0700

    added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		char c;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+
+}
