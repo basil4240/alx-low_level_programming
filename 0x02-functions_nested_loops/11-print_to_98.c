e6db56d8502af032dabccb519bdd90049edc8b48
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-17 21:34:50 -0700

    added task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * @n: Start point
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	for (i = n; i<=98; i++)
+	{
+		if (i != n)
+		{
+			_putchar(',');
+		}
+		if (i >= 10)
+		{
+			int f = i / 10;
+			int l = i % 10;
+
+			_putchar(f + '0');
+			_putchar(l + '0');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+}
+
