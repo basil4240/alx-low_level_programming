d1316c2de71b5b5b1f37640514268d45dfef2de8
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-18 02:54:02 -0700

    made corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,86 @@
-#include "main.h"
+#include <stdlib.h>
+#include "main.h>"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * @n: Start point
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: Start point to print
  *
- * Return: Void
+ * Return: void.
  */
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		if (i != n)
-		{
-			_putchar(',');
-		}
-		if (i >= 10)
-		{
-			int f = i / 10;
-			int l = i % 10;
+		int i;
 
-			_putchar(f + '0');
-			_putchar(l + '0');
-		}
-		else
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
+			oya(n, i);
 		}
 	}
+	else
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			oya(n, i);
+		}
+	}
+
+	_putchar('\n');
 }
+
+/**
+ * oya - prints out formated numbers using n and i
+ * @n: First number
+ * @i: Second number
+ *
+ * Return: void.
+ */
+void oya(int n, int i)
+{
+	if (i != n)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+
+	if (abs(i) >= 10 && abs(i) <=100)
+	{
+		int f = abs(i) / 10;
+		int l = abs(i) % 10;
+
+		if(i < 0)
+		{
+			_putchar('-');
+		}
+		_putchar(f + '0');
+		_putchar(l + '0');
+	}
+	else if (abs(i) >= 100)
+	{
+		int got = abs(i) / 10;
+		int f = got / 10;
+		int m = got % 10;
+		int l = abs(i) % 10;
+
+		if (i < 0)
+		{
+			_putchar('-');
+		}
+		_putchar(f + '0');
+		_putchar(m + '0');
+		_putchar(l + '0');
+	}
+	else
+	{
+		if (i < 0)
+		{
+			_putchar('-');
+		}
+
+		_putchar(abs(i) + '0');
+	}
+}
+
