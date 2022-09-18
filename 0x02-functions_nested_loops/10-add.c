28bc9f27c8cbf75e2d3065f34bcbbf6a9e48a21b
Mc-Ben Ostemobo Ba-ana <basil4240@gmail.com>
2022-09-17 21:19:49 -0700

    added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers a and b and returns the result
+ * @a: First number to add
+ * @b: Second number
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
